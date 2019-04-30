d8a9e995f9441c5b74774fe2957d559064cf634b
NotASithLord <generationapollo@gmail.com>
2019-04-30 15:12:24 -0400

    rearrange layout and begun conversations refactor

--- index.js
+++ index.js
@@ -2,170 +2,32 @@
 "use strict";
 document.title = "D.chat";
 
-const conversations = [];
 
-function getTime() {
-    const date = new Date();
-    return date.getHours() + ":" + date.getMinutes();
-}
-
-function sendMessage(userInput) {
-  console.log("current convo", currentConvo());
-    if (userInput) {
-        try {
-            currentConvo().messageQueue.push({
-            sender: "you",
-            content: userInput,
-            time: getTime(),
-            read: true,
-            img: "assets/vader.jpg"
-          });
-        }
-        catch(error) {
-          console.error(error);
-        }
-    }
-}
-
-function currentConvo() {
-    return conversations.find((convo) => {
-        return convo.convoName === m.route.param('conversation');
-    });
-}
-
-function searchInput(searchTerm, array) {
-    searchTerm = searchTerm.toLowerCase();
-    const searchResults = [];
-
-    if (searchTerm.length) {
-        let reg = new RegExp(`\\b${searchTerm}\\b`);
-        conversations.forEach((convo) => {
-            let match = false;
-            // console.log(convo.sender.toLowerCase(), reg.test(convo.sender.toLowerCase()))
-            if (reg.test(convo.convoName.toLowerCase())) {
-                match = true;
-            }
-            convo.messageQueue.forEach((msg) => {
-                // console.log(msg.content.toLowerCase(), reg.test(msg.content.toLowerCase()))
-                if (reg.test(msg.content.toLowerCase())) {
-                    match = true;
-                }
+//NOTE:Not as fast as jquery onload, since it waits for external assets not just DOM. Find suitable vanilla replacement
+//Only include UI interactivity within here, all other logic can start loading before DOM is ready
+window.onload = () => {
+    // console.log(document.body);
+    m.route(document.body, "/", {
+        "/": {
+            render: () => m(Layout, {
+                homeMsg: m(HomeScreen)
             })
-            if (match) {
-                // console.log(convo);
-                searchResults.push(convo);
-                // console.log(searchResults);
-            }
-        });
-    }
-    return searchResults;
-}
-
-const HomeScreen = {
-    view: () => {
-        return m('h3.welcomeMsg',"Please select a conversation to get started");
-    }
-}
-
-const Chatbox = {
-    view: () => {
-        const convo = currentConvo();
-        convo.messageQueue.forEach(msg => msg.read = true);
-        return convo.messageQueue.map((msg) => {
-            return m('div.msgWrapper', m('div.msg', {
-                class: msg.sender
-            }, [
-                m('div.msgHeader', [
-                    m('img', {
-                        src: msg.img
-                    }),
-                    m('p', msg.sender),
-                    m('p.time', msg.time),
-                ]),
-                m('div.msgContent', [
-                    m('p', msg.content),
-                ]),
-            ]))
-        });
-    }
-}
-
-function MessageComposer() {
-    let userInput = "";
-    return {
-        view: () => {
-            return m("form.messageComposer", {
-                onsubmit(event) {
-                  event.preventDefault();
-                  sendMessage(userInput);
-                  userInput = "";
-                }
-            }, [
-                m("div.accImgWrap", m("img.profileImg", {
-                    src: "assets/vader.jpg",
-                })),
-                m("input[autofocus].msgInput", {
-                    placeholder: "Your message here ...",
-                    value: userInput,
-                    oninput: (event) => {
-                        userInput = event.target.value
-                    },
-                }),
-                m("div.sendButton", [
-                    m("input[type=submit]#submit", {
-                        value: "Send"
-                    }),
-                ]),
-            ]);
-        }
-    }
-}
-
-const ChatPane = {
-    view: (vnode) => {
-        let paneContent = [];
-        if (vnode.attrs.searchResults.length) {
-            paneContent = vnode.attrs.searchResults;
-            // console.log("first", array);
-        } else {
-            paneContent = conversations
-            // console.log("hi", array);
-        }
-        return paneContent.map((convo) => {
-            const preview = convo.messageQueue[convo.messageQueue.length - 1];
-            let unread = 0;
-            convo.messageQueue.forEach((msg) => {
-                if (msg.read === false) unread++;
+        },
+        "/:conversation": {
+            render: () => m(Layout, {
+                messages: Chatbox,
+                composer: m(MessageComposer)
             })
-            return m('a', {
-              href: `/${convo.convoName}`,
-              oncreate: m.route.link
-            }, [
-              m('div.convoWrapper', [
-                m('div.conversation', {
-                  //Solves unread # refresh, feels hacky
-                    onclick() {
-                      m.redraw();
-                    }
-                }, [
-                        m('img', {
-                            src: convo.img
-                        }),
-                        m('h3', convo.convoName),
-                        m('span.previewWrap', [
-                            m('p', preview.content)
-                        ]),
-                        m('span.unread', unread)
-                ])
-            ])
-          ])
-        })
-    }
+        },
+    })
 }
 
 function Layout() {
-    let showHamburger = false;
+    const conversations = [];
     let searchResults = [];
+    let showHamburger = false;
+
+
 
     //This takes the subscription data from window.connect in backend.js, and does the logic to push the formatted incomingMessages into the conversations array
     connect(messageData => {
@@ -228,7 +90,8 @@ function Layout() {
                     }),
                 ]),
                 m("div#chatsPane", m(ChatPane, {
-                    searchResults: searchResults
+                    searchResults: searchResults,
+                    conversations: conversations
                 }))
             ]),
             m("section.outerChat", [
@@ -237,7 +100,7 @@ function Layout() {
                       // console.log("chatbox change firing");
                         dom.scrollTop = dom.scrollHeight;
                     }
-                }, vnode.attrs.messages, vnode.attrs.homeMsg),
+                },  vnode.attrs.messages ? m(vnode.attrs.messages,{conversations: conversations}) : vnode.attrs.messages , vnode.attrs.homeMsg),
                 vnode.attrs.composer
             ])
             ])
@@ -245,23 +108,159 @@ function Layout() {
     }
 }
 
-//NOTE:Not as fast as jquery onload, since it waits for external assets not just DOM. Find suitable vanilla replacement
-//Only include UI interactivity within here, all other logic can start loading before DOM is ready
-window.onload = () => {
-    // console.log(document.body);
-    m.route(document.body, "/", {
-        "/": {
-            render: () => m(Layout, {
-                homeMsg: m(HomeScreen)
-            })
-        },
-        "/:conversation": {
-            render: () => m(Layout, {
-                messages: m(Chatbox),
-                composer: m(MessageComposer)
-            })
-        },
-    })
+const HomeScreen = {
+    view: () => {
+        return m('h3.welcomeMsg',"Please select a conversation to get started");
+    }
 }
+
+const Chatbox = {
+    view: (vnode) => {
+        const convo = currentConvo(vnode.attrs.conversations);
+        convo.messageQueue.forEach(msg => msg.read = true);
+        return convo.messageQueue.map((msg) => {
+            return m('div.msgWrapper', m('div.msg', {
+                class: msg.sender
+            }, [
+                m('div.msgHeader', [
+                    m('img', {
+                        src: msg.img
+                    }),
+                    m('p', msg.sender),
+                    m('p.time', msg.time),
+                ]),
+                m('div.msgContent', [
+                    m('p', msg.content),
+                ]),
+            ]))
+        });
+    }
+}
+
+function MessageComposer() {
+    let userInput = "";
+    return {
+        view: () => {
+            return m("form.messageComposer", {
+                onsubmit(event) {
+                  event.preventDefault();
+                  sendMessage(userInput);
+                  userInput = "";
+                }
+            }, [
+                m("div.accImgWrap", m("img.profileImg", {
+                    src: "assets/vader.jpg",
+                })),
+                m("input[autofocus].msgInput", {
+                    placeholder: "Your message here ...",
+                    value: userInput,
+                    oninput: (event) => {
+                        userInput = event.target.value
+                    },
+                }),
+                m("div.sendButton", [
+                    m("input[type=submit]#submit", {
+                        value: "Send"
+                    }),
+                ]),
+            ]);
+        }
+    }
+}
+
+const ChatPane = {
+    view: (vnode) => {
+        let paneContent = [];
+        if (vnode.attrs.searchResults.length) {
+            paneContent = vnode.attrs.searchResults;
+            // console.log("first", array);
+        } else {
+            paneContent = vnode.attrs.conversations
+            // console.log("hi", array);
+        }
+        return paneContent.map((convo) => {
+            const preview = convo.messageQueue[convo.messageQueue.length - 1];
+            let unread = 0;
+            convo.messageQueue.forEach((msg) => {
+                if (msg.read === false) unread++;
+            })
+            return m('a', {
+              href: `/${convo.convoName}`,
+              oncreate: m.route.link
+            }, [
+              m('div.convoWrapper', [
+                m('div.conversation', [
+                        m('img', {
+                            src: convo.img
+                        }),
+                        m('h3', convo.convoName),
+                        m('span.previewWrap', [
+                            m('p', preview.content)
+                        ]),
+                        m('span.unread', unread)
+                ])
+            ])
+          ])
+        })
+    }
+}
+
+function getTime() {
+    const date = new Date();
+    return date.getHours() + ":" + date.getMinutes();
+}
+
+function sendMessage(userInput) {
+  console.log("current convo", currentConvo());
+    if (userInput) {
+        try {
+            currentConvo().messageQueue.push({
+            sender: "you",
+            content: userInput,
+            time: getTime(),
+            read: true,
+            img: "assets/vader.jpg"
+          });
+        }
+        catch(error) {
+          console.error(error);
+        }
+    }
+}
+
+function currentConvo(convos) {
+    return convos.find((convo) => {
+        return convo.convoName === m.route.param('conversation');
+    });
+}
+
+function searchInput(searchTerm, array) {
+    searchTerm = searchTerm.toLowerCase();
+    const searchResults = [];
+
+    if (searchTerm.length) {
+        const reg = new RegExp(`\\b${searchTerm}\\b`);
+        conversations.forEach((convo) => {
+            let match = false;
+            // console.log(convo.sender.toLowerCase(), reg.test(convo.sender.toLowerCase()))
+            if (reg.test(convo.convoName.toLowerCase())) {
+                match = true;
+            }
+            convo.messageQueue.forEach((msg) => {
+                // console.log(msg.content.toLowerCase(), reg.test(msg.content.toLowerCase()))
+                if (reg.test(msg.content.toLowerCase())) {
+                    match = true;
+                }
+            })
+            if (match) {
+                // console.log(convo);
+                searchResults.push(convo);
+                // console.log(searchResults);
+            }
+        });
+    }
+    return searchResults;
+}
+
 
 })();
